a4fbc8431cb24cf635d041fa0d59f4a4cbb5670c
Mustafa Ozzengin <myozzengin@gmail.com>
2022-10-03 04:50:25 +0200

    03/Oct/2022-04:50-AM

--- src/test/java/tests/day16_Priority_DependsOn/C04_DependsOnMethod.java
+++ src/test/java/tests/day16_Priority_DependsOn/C04_DependsOnMethod.java
@@ -1,6 +1,8 @@
 package tests.day16_Priority_DependsOn;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import utilities.TestBaseBeforeClassAfterClass;
@@ -9,22 +11,23 @@ public class C04_DependsOnMethod extends TestBaseBeforeClassAfterClass {
 
     @Test
     public void test1() {
-        //amazon anasayfasina gidiniz
-        driver.get("https://www.amazon.com");
+        // amazon ana sayfasına gidelim
+        driver.get("https://www.Wamazon1.com");
     }
 
     @Test(dependsOnMethods = "test1")
     public void test2() {
-        // nutella aratalim
-        WebElement sonuc = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
-
+        //Nutella aratalım
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Nutella", Keys.ENTER);
     }
 
     @Test(dependsOnMethods = "test2")
     public void test3() {
-        // sonuc yazisinin amazon icerdiginiz test edin
+        //Sonuc yazısının amazon içerdiğini test edelim
+        WebElement sonuc = driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertFalse(sonuc.getText().contains("amazon"));
         //assert ! driver.findElement
-        //        (By.cssSelector("div[class=\"a-section a-spacing-small a-spacing-top-small\"]")).getText().contains("amazon");
-        // Assert.assertFalse(sonuc.getText().contains("amazon"));
+//        (By.cssSelector("div[class=\"a-section a-spacing-small a-spacing-top-small\"]")).getText().contains("amazon");
+
     }
 }
